21fb5ca92d32ab7ee2949de383b9f5243b890cdb
Taras Synytsia <s.taras.b@gmail.com>
2017-10-22 20:11:52 +0300

    initial commit

--- src/test/java/HomeTask1.java
+++ src/test/java/HomeTask1.java
@@ -1,6 +1,8 @@
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.EdgeDriverManager;
 import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
+import org.apache.commons.lang3.SystemUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,7 +11,12 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariDriverService;
 
+import static org.junit.Assume.assumeThat;
+
 public class HomeTask1 {
+
+    String os = System.getProperty("os.name");/*.toLowerCase();*/
+
 
     @Test
     public void testChrome() {
@@ -31,30 +38,40 @@ public class HomeTask1 {
         driver.quit();
     }
 
+
     @Test
     public void testEdge() {
-        EdgeDriverManager.getInstance().setup();
-        WebDriver driver = new EdgeDriver();
-        driver.get("http://google.com/ncr");
+        if (os.contains("Windows 10")){
 
-        driver.quit();
+            EdgeDriverManager.getInstance().setup();
+            WebDriver driver = new EdgeDriver();
+            driver.get("http://google.com/ncr");
+
+            driver.quit();
+        }
+        else System.out.println("OS is unsupportable");
     }
 
     @Test
     public void testIE() {
+        if (os.contains("Windows 10")){
         InternetExplorerDriverManager.getInstance().setup();
         WebDriver driver = new InternetExplorerDriver();
         driver.get("http://google.com/ncr");
 
         driver.quit();
+        }
+        else System.out.println("OS is unsupportable");
     }
 
-    /*@Test
+    @Test
     public void testSafari() {
-
+        if (os.contains("Mac OS X")){
         WebDriver driver = new SafariDriver();
         driver.get("http://google.com/ncr");
 
         driver.quit();
-    }*/
+        }
+        else System.out.println("OS is unsupportable");
+    }
 }
